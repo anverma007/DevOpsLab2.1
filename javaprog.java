0c0ae00e5c130d7578a520e812a9fca09ec3ba14
anverma007 <56126665+anverma007@users.noreply.github.com>
2019-10-04 10:17:28 +0530

    Updated JavaProg

--- javaprog.java
+++ javaprog.java
@@ -1,10 +1,10 @@
 class Javaprog {
 	
-public static void main(String [] args){
+	public static void main(String [] args){
 
-	System.out.println("Hello World");
-
-}
-
-
+		for(int i=0; i<10; i++)
+		{
+			System.out.println(i);
+		}
+	}
 }
